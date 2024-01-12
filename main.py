06e31456e62b5694b0faa177db4b9fb42fd9b0fb
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-01-12 22:37:32 +0545

    Add feature to pass command line argument to supply input

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-from src import mac, linux 
+from src import mac, linux
 import platform
+import sys
 
 def main():
+    if len(sys.argv) < 3:
+        print("Must provide input file, output file(no extension)")
+        sys.exit()
+    print(sys.argv)
+    print("Press enter to continue:")
     if platform.system() == 'Linux':
-        out = linux.Autotyper('main.py', title='autotype')
+        out = linux.Autotyper(*sys.argv[1:])
     elif platform.system() == "Darwin":
-        out = mac.Autotyper("main.py", title="autotype")
+        out = mac.Autotyper(*sys.argv[1:])
     else:
         print("Unknow system")
-        import sys
         sys.exit()
 
     try:
         out.run()
     except Exception as error:
         print(error)
-
 
 if __name__ == "__main__":
     main()
--- src/mac.py
+++ src/mac.py
@@ -4,6 +4,7 @@ import sys
 import re
 from pynput.keyboard import Key, Listener
 import time
+from random import randint
 
 def process_char(char):
     if char == '<':
@@ -12,7 +13,7 @@ def process_char(char):
         pyautogui.write(char)
 
 def launch_temrinal():
-    return os.system('open -a Terminal')  # Modified for macOS
+    return os.system('open -a Terminal')
 
 def fullscreen():
     pyautogui.press("f11")
@@ -34,22 +35,26 @@ def is_valid_file(filename):
     return True
 
 class Autotyper:
-    NO_DELAY  = 0.0
-    FAST_TYPE = 0.01
-    NORMAL_TYPE = 0.05
-    SLOW_TYPE = 0.1
-    def __init__(self, file_path, title):
+    TYPE_SPEED = (0.0, 0.01, 0.05, 0.1)
+    AVILABLE_SPEED_COUNT = len(TYPE_SPEED)
+    def __init__(self, file_path, output_filename, speed=0):
         if not is_valid_file(file_path):
             print("File path doesnt exist\nQuitting....")
             sys.exit()
-
+        try:
+            speed = abs(int(speed))
+        except ValueError:
+            print("Expected integer")
+            sys.exit()
+        
         self.file = open(file_path, mode='r')
-        self.output_file = title.strip() + os.path.splitext(file_path)[1]
+        self.output_file = output_filename.strip() + os.path.splitext(file_path)[1]
         self.start = False
         self.listener = None
+        self.speed = speed % self.AVILABLE_SPEED_COUNT if speed < 4 else random.choice(self.TYPE_SPEED)
 
         self.save_commands = [ ['ctrl', 's'], ['ctrl', 'x'] ]
-    
+
     def type_file_content(self, interval):
         while a := self.file.readline():
             indexs_lt_braces = list( map( lambda x: x.span(), list(re.finditer('<', a))) )
@@ -70,18 +75,20 @@ class Autotyper:
     def run(self):
         with Listener(on_press = self.detect_start) as self.listener:
             self.listener.join()
-        
+
         if launch_temrinal() != 0 >> 8:
             print("Failed to open terminal")
             sys.exit()
 
         if os.path.exists(self.output_file):
             os.remove(self.output_file)
+
+        fullscreen()
         set_command(f"nano {self.output_file}")
-        self.type_file_content(self.NO_DELAY)
+        self.type_file_content(self.speed)
         save_file(self.save_commands)
-        set_command("exit")
         self.destruct()
 
     def destruct(self):
         self.file.close()
+
